d4cff3a425890731419505506e78ce70940908ad
Diego Akechi <dakechi@suse.com>
2019-05-20 10:30:44 +0200

    Rename metrics and include more meaninful descriptions.

--- hanadb_exporter/exporters/prometheus_metrics.py
+++ hanadb_exporter/exporters/prometheus_metrics.py
@@ -18,18 +18,18 @@ except ImportError:
 
 METRICS = [
     {
-        'query': 'SELECT ROUND(SUM(TOTAL_MEMORY_USED_SIZE/1024/1024/1024), 2) AS "Used Memory GB" FROM M_SERVICE_MEMORY;',
-        'info': ('used_memory', 'Use memory in GB', None, [], 'GB'),
+        'query': 'SELECT ROUND(SUM(TOTAL_MEMORY_USED_SIZE/1024/1024), 2) AS "Used Memory MB" FROM M_SERVICE_MEMORY;',
+        'info': ('hanadb_total_used_memory', 'Total used memory in MB', None, [], 'MB'),
         'type': core.GaugeMetricFamily
     },
     {
         'query': 'SELECT ROUND(SUM(MEMORY_SIZE_IN_TOTAL)/1024/1024) AS "Column Tables MB Used" FROM M_CS_TABLES;',
-        'info': ('column_tables_memory', 'Column Tables MB Used', None, [], 'MB'),
+        'info': ('hanadb_column_tables_used_memory', 'Column tables total memory used in MB', None, [], 'MB'),
         'type': core.GaugeMetricFamily
     },
     {
         'query': 'SELECT SCHEMA_NAME AS "Schema", ROUND(SUM(MEMORY_SIZE_IN_TOTAL)/1024/1024) AS "MB Used" FROM M_CS_TABLES GROUP BY SCHEMA_NAME ORDER BY "MB Used" DESC;',
-        'info': ('system_data', 'System data', None, ['metric', 'data'], 'MB'),
+        'info': ('hanadb_schema_used_memory', 'Total used memory by schema', None, ['schema', 'data'], 'MB'),
         'type': core.GaugeMetricFamily
     },
 ]
