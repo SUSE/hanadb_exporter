67fd882823f565632383fb037d4e6dea78168093
ayoub-belarbi <belarbi.ayoub@gmail.com>
2019-05-22 15:25:46 +0200

    Fix metrics queries

--- hanadb_exporter/exporters/prometheus_metrics.py
+++ hanadb_exporter/exporters/prometheus_metrics.py
@@ -8,6 +8,8 @@ SAP HANA database prometheus data exporter metrics
 :since: 2019-05-09
 """
 
+import json
+
 # TODO: In order to avoid dependencies, import custom prometheus client
 try:
     from prometheus_client import core
@@ -15,22 +17,11 @@ except ImportError:
     # Load custom prometheus client
     raise NotImplementedError('custom prometheus client not implemented')
 
+class PrometheusMetrics:
+    def __init__(self):
+        self.metrics = self.load_metrics()
 
-METRICS = [
-    # Memory Metrics
-    {
-        'query': 'SELECT ROUND(SUM(total_memory_used_size/1024/1024),2) used_memory_mb FROM m_service_memory;',
-        'info': ('hanadb_total_used_memory', 'Total used memory in MB', None, [], 'MB'),
-        'type': core.GaugeMetricFamily
-    },
-    {
-        'query': 'SELECT ROUND(SUM(memory_size_in_total)/1024/1024) column_tables_used_mb FROM m_cs_tables;',
-        'info': ('hanadb_column_tables_used_memory', 'Column tables total memory used in MB', None, [], 'MB'),
-        'type': core.GaugeMetricFamily
-    },
-    {
-        'query': ' SELECT schema_name, ROUND(SUM(memory_size_in_total)/1024/1024) schema_memory_used_mb FROM m_cs_tables GROUP BY schema_name;',
-        'info': ('hanadb_schema_used_memory', 'Total used memory by schema', None, ['schema', 'data'], 'MB'),
-        'type': core.GaugeMetricFamily
-    },
-]
+    def load_metrics(self):
+        with open('metrics.json') as metrics_file:
+            data = json.load(metrics_file)
+            return data
