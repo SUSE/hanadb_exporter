5d47512915914e49fc18f6e8c0eb8780e225be88
ayoub-belarbi <belarbi.ayoub@gmail.com>
2019-06-04 15:01:47 +0200

    Adapt the collector to the new json structure

--- hanadb_exporter/exporters/prometheus_exporter.py
+++ hanadb_exporter/exporters/prometheus_exporter.py
@@ -37,7 +37,7 @@ class SapHanaCollector(object):
         self._logger = logging.getLogger(__name__)
         self._hdb_connector = conector
 
-    def _execute(self, metric_name, metric):
+    def _execute(self, query, metric_name, metric_data):
         """
         Create metric object
 
@@ -45,30 +45,30 @@ class SapHanaCollector(object):
             metric (dict): query, info, type structure dictionary
         """
         try:
-            query_result = self._hdb_connector.query(metric['query'])
-            if metric['type'] == "gauge":
+            query_result = self._hdb_connector.query(query)
+            if metric_data['type'] == "gauge":
                 query_columns = []
                 metric_dict = {metric_name: []}
                 for meta in query_result.metadata:
                     query_columns.append(meta[0])
                 for record in query_result.records:
                     metric_dict[metric_name].append(list(itertools.izip(query_columns, record)))
-                metric_obj = self._manage_gauge(metric_name, metric, query_result.records)
+                metric_obj = self._manage_gauge(metric_name, metric_data, query_result.records)
             else:
                 raise NotImplementedError('{} type not implemented'.format(metric['type']))
             return metric_obj
         except KeyError as err:
             raise MalformedMetric(err)
 
-    def _manage_gauge(self, metric_name, metric, value):
+    def _manage_gauge(self, metric_name, metric, records):
         """
         Manage Gauge type metric
         """
         # Label not set
         metric_obj = core.GaugeMetricFamily(metric_name, metric['description'], None, metric['labels'], metric['unit'])
 
-        metric_obj.add_metric(metric['labels'], str(value[0][-1]))
-        for label_item in value:
+        metric_obj.add_metric(metric['labels'], str(records[0][-1]))
+        for label_item in records:
             self._logger.info('%s' % (label_item[0]))
 
         return metric_obj
@@ -78,6 +78,7 @@ class SapHanaCollector(object):
         Collect data from database
         """
         metrics = PrometheusMetrics()
-        for metric_name, metric_data in metrics.data.items():
-            metric_obj = self._execute(metric_name, metric_data)
-            yield metric_obj
+        for query, metric in metrics.data.items():
+            for metric_name, metric_data in metric.items():
+                metric_obj = self._execute(query, metric_name, metric_data)
+                yield metric_obj
