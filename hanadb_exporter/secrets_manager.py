c3eb72b4c77b1b345976f977ab968e5ccd5d3975
Samuel CHNIBER <schniber@amazon.fr>
2023-04-06 14:23:58 +0200

    update of exporter ci and enhancement of secrets_manager.py

--- hanadb_exporter/secrets_manager.py
+++ hanadb_exporter/secrets_manager.py
@@ -55,17 +55,22 @@ def get_db_credentials(secret_name):
 
     # Create a Secrets Manager client
     session = boto3.session.Session()
-    client = session.client(service_name="secretsmanager", region_name=region_name)
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
 
     # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
     # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
     # We rethrow the exception by default.
 
     try:
-        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
     except ClientError as e:
         raise SecretsManagerError("Couldn't retrieve secret details") from e
     else:
         # Decrypts secret using the associated KMS CMK.]
-        secret = get_secret_value_response["SecretString"]
+        secret = get_secret_value_response['SecretString']
         return json.loads(secret)
